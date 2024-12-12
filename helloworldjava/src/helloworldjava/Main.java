b53c1b3f600ce6919fbcffc405675bd9dc100eb4
nzktmhr <ncore1400@gmail.com>
2024-12-12 13:17:57 +0900

    Fixed in line with Lint tool

--- helloworldjava/src/helloworldjava/Main.java
+++ helloworldjava/src/helloworldjava/Main.java
@@ -1,7 +1,16 @@
 package helloworldjava;
 
+/**
+ * MAINクラス.
+ */
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
+  
+  /**
+   * メインメソッド.
+   *
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
 }
